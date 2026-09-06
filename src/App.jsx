a029cc66177d6ccde5727f23d2f72338f70e7dc1
Chalith Liyanage <chalith92@gmail.com>
2026-09-06 20:27:04 +0300

    implement header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Header from  './Component/Header'
+
 function App() {
   
 
   return (
-    <>
- 
-    </>
+  
+    <Header></Header>
+  
   )
 }
 
---
+++ src/Component/Header.jsx
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <h1>BMI-Calculator</h1>
+    );
+}
+
+
+export default Header;
